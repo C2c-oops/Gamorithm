76982f5281f42835e757b2498f5c4bd738ef9d5d
Sahil Singh <singhsittu430@gmail.com>
2021-09-08 14:00:27 +0530

    :heavy_plus_sign: Added: Sanke game

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public', {
+	index: false,
+	immutable: true,
+	cacheControl: true,
+	maxAge: "30d"
+}));
+
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
+});
+
+app.get("/snake", (req, res) => {
+	res.sendFile(__dirname + "/public/views/snake.html");
 })
